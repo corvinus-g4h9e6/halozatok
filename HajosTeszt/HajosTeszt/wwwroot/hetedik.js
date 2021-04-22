8498b9b2d8d515abb2134e3eb121aa28d6849eae
Bence Kiss <bence.kiss4@stud.uni-corvinus.hu>
2021-04-22 15:20:17 +0200

    API összes kérdés (9.gyak)

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,5 +1,6 @@
 ﻿var kérdések;
-var kérdésszám = 0;
+var aktualkerdes;
+var kérdésszám = 1;
 
 window.onload = function() {
     letöltés();
@@ -9,7 +10,7 @@ window.onload = function() {
 
 function letöltés() {
 
-    fetch('/questions.json').then(response => response.json()).then(data => letöltésBefejeződött(data))
+   /* fetch('/questions.json').then(response => response.json()).then(data => letöltésBefejeződött(data))
 
     function letöltésBefejeződött(d) {
 
@@ -18,14 +19,21 @@ function letöltés() {
         kérdések = d;
         kérdésMegjelenítés(kérdésszám);
 
-    }
+    }*/
+
+    fetch('/questions/1')
+        .then(response => response.json())
+        .then(data => kérdésMegjelenítés(data)
+        );
+
+
 }
 
 
 
 function kérdésMegjelenítés(kérdés) {
 
-    document.getElementById("kérdés_szöveg").innerHTML = kérdések[kérdés].questionText;
+   /* document.getElementById("kérdés_szöveg").innerHTML = kérdések[kérdés].questionText;
     document.getElementById("válasz1").innerHTML = kérdések[kérdés].answer1;
     document.getElementById("válasz2").innerHTML = kérdések[kérdés].answer2;
     document.getElementById("válasz3").innerHTML = kérdések[kérdés].answer3;
@@ -35,14 +43,28 @@ function kérdésMegjelenítés(kérdés) {
     }
     else {
         document.getElementById("kép1").src = "";
+    }*/
+    aktualkerdes = kérdés;
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    if (kérdés.image != "") {
+        document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+
     }
+    else {
+        document.getElementById("kép1").src = "";
+    }
+
 }
 
 
 
 function Vissza() {
 
-    if (kérdésszám==0) {
+   /* if (kérdésszám==0) {
         kérdésszám = kérdések.length-1;
         letöltés();
         Clear();
@@ -51,21 +73,43 @@ function Vissza() {
         kérdésszám--;
         letöltés();
         Clear();
+    }*/
+    if (kérdésszám==1) {
+        kérdésszám = 859;
     }
+    else {
+        kérdésszám--;
+    }
+   
+    kérdésBetöltés(kérdésszám);
+    Clear();
+
 }
 
 function Elore() {
-    if (kérdésszám == kérdések.length-1) {
+   /* if (kérdésszám == kérdések.length-1) {
         kérdésszám = 0;
-        letöltés();
-        Clear();
+       
     }
     else {
         kérdésszám++;
         letöltés();
-        Clear();
+        
+    }*/
+
+    if (kérdésszám == 859) {
+        kérdésszám = 1;
     }
+    else {
+        kérdésszám++;
+    }
+
+    kérdésBetöltés(kérdésszám);
+    Clear();
+
+  
    
+
 }
 
 
@@ -87,7 +131,8 @@ function valasz3Click() {
 
 
 function valaszCheck(n) {
-    if (n==kérdések[kérdésszám].correctAnswer) {
+   
+    if (n==aktualkerdes.correctAnswer) {
         document.getElementById("válasz" + n).classList.add("jo");
     }
     else {
@@ -104,3 +149,17 @@ function Clear() {
     document.getElementById("válasz3").classList.remove("jo");
     document.getElementById("válasz3").classList.remove("rossz");
 }
+
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}    
